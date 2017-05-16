f90558c38d9b2b10427273ab04c976e1de6f1425
Malcolm Daigle <daiglemalcolm@gmail.com>
2017-05-15 20:14:18 -0700

    Adds trusted filter tool.

--- src/TrustedFilterEqual.java
+++ src/TrustedFilterEqual.java
@@ -1,4 +1,5 @@
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 /**
  * Created by mdaigle on 5/10/17.
@@ -6,15 +7,28 @@ import java.sql.ResultSet;
 public class TrustedFilterEqual <T> extends Tool{
     private Table inputTable;
     private String outputTableName;
-    private ResultSet
+    private ResultSet resultSet;
+    private Predicate predicate;
 
-    public TrustedFilterEqual(String toolName, int numTables, Table inputTable, String outputTableName) {
+    public TrustedFilterEqual(String toolName, int numTables, Table inputTable, String outputTableName, Predicate predicate) {
         super(toolName, numTables);
         this.inputTable = inputTable;
         this.outputTableName = outputTableName;
+        this.predicate = predicate;
     }
 
-    public getNext() {
+    public ArrayList<Row> run() {
+        ArrayList<Tuple> inputRows = inputTable.getRows();
+        ArrayList<Row> outputRows = new ArrayList<>();
 
+        for(Tuple t : inputRows) {
+            if (predicate.filter(t)) {
+                RowProvenance rp = new RowProvenance(new Table[]{inputTable});
+                Row r = new Row(t, rp);
+                outputRows.add(r);
+            }
+        }
+
+        return outputRows;
     }
 }
