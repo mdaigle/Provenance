06bba9ec65f548ca03cff6a90874555dafb99cb5
Malcolm Daigle <daiglemalcolm@gmail.com>
2017-05-24 15:14:52 -0700

    Run tools in separate process.

--- src/Tool.java
+++ src/Tool.java
@@ -1,7 +1,17 @@
+import java.io.*;
+
 /**
  * Created by mdaigle on 4/5/17.
  */
 public class Tool {
+    private static final String DEFAULT_TOOL_FILE_EXTENSION = ".jar";
+    private static final String DEFAULT_TOOL_START_COMMAND = "java -jar";
+
+    /**
+     * The id of this tool.
+     */
+    private int toolId;
+
     /**
      * The name of this tool.
      */
@@ -20,6 +30,59 @@ public class Tool {
     public Tool(String name, int numTables) {
         this.name = name;
         this.numTables = numTables;
+    }
+
+    public String run(Table[] inputTables, Parameter[] params) {
+        String metadata = this.generateMetadata(inputTables, params);
+        String[] CSVs = new String[inputTables.length];
+        for (int i = 0; i < inputTables.length; i++) {
+            CSVs[i] = inputTables[i].getCSV();
+        }
+
+        String startCommand = DEFAULT_TOOL_START_COMMAND;
+        String fileName = this.name + DEFAULT_TOOL_FILE_EXTENSION;
+        String args = String.join(" ", CSVs);
+
+        String fullCommand = startCommand + " " + fileName + " " + args;
+
+        System.out.println(fullCommand);
+
+        String outputCSV = "";
+        // Run the tool in a separate system process
+        try {
+            Process proc = Runtime.getRuntime().exec(fullCommand);
+
+            InputStream in = proc.getInputStream();
+            InputStream err = proc.getErrorStream();
+            OutputStream out = proc.getOutputStream();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder builder = new StringBuilder();
+
+            String line;
+            do {
+                line = reader.readLine();
+                builder.append(line != null ? line : "");
+            }
+            while (line != null);
+
+            outputCSV = builder.toString();
+
+
+        } catch (IOException e) {
+            System.out.println("Error running tool");
+        }
+
+        return outputCSV;
+    }
+
+    private String generateMetadata(Table[] inputTables, Parameter[] params) {
+        String[] inputTableIds = new String[inputTables.length];
+        for (int i = 0; i < inputTableIds.length; i++) {
+            inputTableIds[i] = inputTables[i].getId();
+        }
+        TableMetadata metadata = new TableMetadata(this.toolId, inputTableIds, params);
+        return metadata.toJSON();
     }
 
     /**
